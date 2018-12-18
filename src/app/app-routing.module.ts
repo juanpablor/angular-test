8c266595d535f459e2b83fa4ff2f26fac275d6d3
jpriano <juanpablo@riano.ca>
2018-12-18 15:26:40 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DescriptionComponent } from './pages/description/description.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: PortfolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'description', component: DescriptionComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { DescriptionComponent } from './pages/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent,
+    AboutComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
